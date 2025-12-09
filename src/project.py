0f57ed2340f4c09a8551a20c6fa656cc613c53c8
rmurph19 <ryanalan2015@icloud.com>
2025-12-08 19:23:59 -0600

    Created a delta time variable and Player Class with init function.

--- src/project.py
+++ src/project.py
@@ -8,6 +8,16 @@ screen = pygame.display.set_mode((Width, Height), pygame.RESIZABLE)
 pygame.display.set_caption('Shapes in Time')
 clock = pygame.time.Clock()
 
+
+class Player:
+    def __init__(self):
+        self.x = Width // 2
+        self.y = Height // 2
+        self.speed = 50
+        self.shape = "circle"
+        self.size = 30
+        self.health = 100
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -16,4 +26,4 @@ while True:
 
 
     pygame.display.update()
-    clock.tick(60)
+    dt = clock.tick(60)/1000
